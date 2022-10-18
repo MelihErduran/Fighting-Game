bddeed4fb686b3d9cda7f75915bc7650496653ab
Melih Erduran <merduran.ed@gmail.com>
2022-10-18 12:59:34 -0700

    Adding Movement

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ class Sprite {
 
     draw() {
         c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, 150, this.height)
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
     }
 
     update() {
@@ -26,7 +26,7 @@ class Sprite {
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
-        if (this.position.y + this.height + this.velocity >= canvas.height) {
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
             this.velocity.y = 0
         } else this.velocity.y += gravity
     }
@@ -55,12 +55,29 @@ const enemy = new Sprite({
     }
 })
 
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+let lastKey
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
+
+    player.velocity.x = 0
+    if (keys.a.pressed && lastKey === 'a') {
+        player.velocity.x = -1
+    } else if (keys.d.pressed && lastKey === 'd') {
+        player.velocity.x = 1
+    }
 }
 
 animate();
@@ -68,16 +85,24 @@ animate();
 window.addEventListener('keydown', (event) => {
     switch (event.key) {
         case 'd':
-            player.velocity.x = 1
+            keys.d.pressed = true
+            lastKey = 'd'
+            break
+        case 'a':
+            keys.a.pressed = true
+            lastKey = 'a'
             break
     }
     console.log(event.key)
 })
 
-window.addEventListener('keydown', (event) => {
+window.addEventListener('keyup', (event) => {
     switch (event.key) {
         case 'd':
-            player.velocity.x = 1
+            keys.d.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
             break
     }
     console.log(event.key)
